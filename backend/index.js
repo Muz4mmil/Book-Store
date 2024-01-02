3e32c9221fd8bef65d564be596597b4f27e7a2a9
Muzammil <muzammilsiddiqui826@gmail.com>
2024-01-02 15:39:55 +0530

    Deploy backend

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ import cors from 'cors'
 const app = express();
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://book-store-ten-opal.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  }))
 
 app.get("/", (req, res)=>{
     return res.status(234).send("<div>Hello MERN</div>")
